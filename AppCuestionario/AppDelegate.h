2d241e14267255fba2e5447a46438d43fc78cadd
Marcos JRS <marcosjrs80@gmail.com>
2017-07-31 18:39:42 +0200

    Inicio

---
+++ AppCuestionario/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppCuestionario
+//
+//  Created by Marcos JRS on 31/7/17.
+//  Copyright © 2017 Marcos JRS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppCuestionario/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppCuestionario
+//
+//  Created by Marcos JRS on 31/7/17.
+//  Copyright © 2017 Marcos JRS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
