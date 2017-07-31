47af29763d0c532887b2a7cb70b697a0b6c9d240
Marcos JRS <marcosjrs80@gmail.com>
2017-07-31 19:03:11 +0200

    Añadimos parte del modelo. Preguntas y respuestas.

--- AppCuestionario/ViewController.h
+++ AppCuestionario/ViewController.h
@@ -10,6 +10,10 @@
 
 @interface ViewController : UIViewController
 
+@property (strong, nonatomic) NSArray * preguntas;
+@property (strong, nonatomic) NSArray * respuestas;
+@property NSUInteger indice;
+
 
 @end
 
--- AppCuestionario/ViewController.m
+++ AppCuestionario/ViewController.m
@@ -16,7 +16,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    _indice = 0;
+    _preguntas = @[ @"¿Cuándo se instaló el primer semáforo?",
+                    @"¿Cuánto cuesta fabricar un euro?",
+                    @"¿Qué es iOS?", @"¿Qué es MVC?",
+                    @"¿Qué es Objective C?" ];
+    
+    _respuestas = @[ @"El primer semáforo data de 1868",
+                     @"Las monedas de un euro cuestan en torno a 4,5 céntimos. Las de dos euros rondan los 5,2 céntimos. Las más pequeñas, de 1 y 2 céntimos, salen prácticamente por lo mismo que valen, por lo que se ha puesto en duda lo oportuno de seguir fabricándolas.",
+                     @"El Sistema Operativo de iPhone e iPad",
+                     @"Model-View-Controller\nModelo-Vista-Controlador",
+                     @"Un superconjunto estricto de C" ];
+    
 }
 
 
